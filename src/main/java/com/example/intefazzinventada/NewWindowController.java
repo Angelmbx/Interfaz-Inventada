576620869478b9d81c750c5a2180cf53d368e8ea
Ángel MB <114161450+Angelmbx@users.noreply.github.com>
2023-03-08 17:52:41 +0100

    Cambiar texto solucionado

--- src/main/java/com/example/intefazzinventada/NewWindowController.java
+++ src/main/java/com/example/intefazzinventada/NewWindowController.java
@@ -5,9 +5,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class NewWindowController {
+  @FXML
+    public GridPane abc;
+
     @FXML
     private Button btMostrarDatos;
 
+    private Usuario user;
     @FXML
     private Label lbNombre;
     @FXML
@@ -16,8 +20,21 @@ public class NewWindowController {
     private Label lbTelf;
 
 
+    //en VentanaNuevaController. Método que recupera los datos introducidos en la ventana anterior.
+
+
     @FXML
     protected void onMostrarDatosClick(){
+
+        Stage stage = (Stage) this.abc.getScene().getWindow();   //Me devuelve la ventana
+        user = (Usuario) stage.getUserData();
+        System.out.println(user.toString());
+
+        lbNombre.setText(user.getNombre());
+        lbApellidos.setText(user.getApellidos());
+        lbTelf.setText(user.getTelf());
+
+
 
     }
 }
